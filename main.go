18b6355a83569961ca961580cfa38d507ca6bf66
peishaopeng <peishaopeng@domain.com>
2025-08-07 17:22:16 +0800

    init

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "meow-bot",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		Mac: &mac.Options{
+			// 在 Mac 上，需要额外的设置来确保窗口内容区域透明
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			TitleBar:             mac.TitleBarHidden(),
+		},
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
